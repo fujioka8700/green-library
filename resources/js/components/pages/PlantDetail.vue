67fff0094659977c1f0e6715b74292ef0c4598c0
Masahiro Fujioka <fujioka8700@gmail.com>
2022-08-28 09:18:14 +0900

    [updata] 植物詳細ページにデータを送り込む

--- resources/js/components/pages/PlantDetail.vue
+++ resources/js/components/pages/PlantDetail.vue
@@ -15,44 +15,74 @@
             <tbody>
               <tr>
                 <th scope="row">植物名</th>
-                <td>アネモネ・パルマータ</td>
+                <td v-if="plant">{{ plant.name }}</td>
               </tr>
               <tr>
                 <th scope="row">学名</th>
-                <td>Anemone palmata</td>
+                <td></td>
               </tr>
               <tr>
                 <th scope="row">科名</th>
-                <td>キンポウゲ科</td>
+                <td></td>
               </tr>
               <tr>
                 <th scope="row">属名</th>
-                <td>イチリンソウ属</td>
+                <td></td>
               </tr>
               <tr>
                 <th scope="row">原産地</th>
-                <td>ヨーロッパ</td>
+                <td></td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
-      <h2>アネモネ・パルマータの特徴</h2>
-      <p>
-        アネモネ・パルマータは、キンポウゲ科の原種のアネモネのひとつです。アネモネというと球根の花が一般的ですが、パルマータは宿根草です。宿根草タイプのアネモネの中では小型で、かわいらしいサイズです。
-        花の咲く時期以外は下葉のみ、春になると株元から花茎が立ち上がり、晩春から初夏にクリームイエローの花が開花します。咲き始めはクリームイエロー、開くとクリーム色になり、華やかな色合いのアネモネの中では素朴な印象です。花後の種が入る実は、まん丸でかわいい形をしていて観賞価値があります。
-        酷暑と表現される最近の夏を越せることができれば、基本的には植えっぱなしにできる宿根草です。
+      <h2 v-if="plant">{{ plant.name }}の特徴</h2>
+      <p v-if="plant">
+        {{ plant.body }}
       </p>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from "axios";
+  import {
+    OK,
+  } from '../../util';
+
   export default {
     props: {
       id: {
         type: String,
         required: true,
+      },
+    },
+    data() {
+      return {
+        plant: null,
+      }
+    },
+    methods: {
+      async fetchPlant() {
+        const response = await axios.get(`/api/plants/${this.id}`);
+
+        if (response.status !== OK) {
+          this.$store.commit('error/setCode', response.status);
+          return false;
+        }
+
+        if (response.data.data !== null) {
+          this.plant = response.data.data;
+        }
+      },
+    },
+    watch: {
+      $route: {
+        async handler() {
+          await this.fetchPlant();
+        },
+        immediate: true,
       },
     },
   }
